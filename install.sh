4f63d66f6e2fd9f9e3d3f4605d12110277c0dc1f
ICEmongol <kalybeksybankul@gmail.com>
2026-03-15 19:30:46 +0500

    added new install.sh and changed polybar

--- install.sh
+++ install.sh
@@ -1,22 +1,142 @@
 #!/bin/bash
 
-echo "Installing ExelIce Arch i3 rice..."
+set -e
 
-# user configs
-echo "Installing user configs..."
+echo "Installing ExelIce Arch i3 Rice..."
+
+########################################
+# UPDATE SYSTEM
+########################################
+
+echo "Updating system..."
+sudo pacman -Syu --noconfirm
+
+########################################
+# CORE PACMAN PACKAGES
+########################################
+
+echo "Installing core packages..."
+
+PACMAN_PACKAGES=(
+i3-wm
+polybar
+rofi
+picom
+dunst
+alacritty
+feh
+xss-lock
+flameshot
+brightnessctl
+dex
+thunar
+thunar-archive-plugin
+networkmanager
+network-manager-applet
+bluez
+bluez-utils
+blueman
+pipewire
+pipewire-pulse
+wireplumber
+pavucontrol
+lxqt-policykit
+ttf-jetbrains-mono-nerd
+xorg-server
+xorg-xinit
+greetd
+greetd-tuigreet
+)
+
+sudo pacman -S --needed --noconfirm "${PACMAN_PACKAGES[@]}"
+
+########################################
+# INSTALL YAY (AUR HELPER)
+########################################
+
+if ! command -v yay &> /dev/null
+then
+    echo "Installing yay..."
+
+    sudo pacman -S --needed --noconfirm git base-devel
+
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si --noconfirm
+    cd ..
+    rm -rf yay
+fi
+
+########################################
+# AUR PACKAGES
+########################################
+
+echo "Installing AUR packages..."
+
+AUR_PACKAGES=(
+i3lock-color
+)
+
+yay -S --needed --noconfirm "${AUR_PACKAGES[@]}"
+
+########################################
+# ENABLE SERVICES
+########################################
+
+echo "Enabling services..."
+
+sudo systemctl enable NetworkManager
+sudo systemctl enable bluetooth
+sudo systemctl enable greetd
+
+########################################
+# LINK DOTFILES
+########################################
+
+echo "Linking dotfiles..."
+
+DOTFILES="$(cd "$(dirname "$0")" && pwd)"
+
+CONFIGS=(
+i3
+polybar
+rofi
+picom
+alacritty
+dunst
+)
+
 mkdir -p ~/.config
-cp -r .config/* ~/.config/
-cp .xinitrc ~
 
-# X11 configs
+for config in "${CONFIGS[@]}"; do
+
+TARGET="$HOME/.config/$config"
+SOURCE="$DOTFILES/.config/$config"
+
+rm -rf "$TARGET"
+ln -s "$SOURCE" "$TARGET"
+
+done
+
+########################################
+# INSTALL X11 CONFIGS
+########################################
+
 echo "Installing X11 configs..."
+
 sudo mkdir -p /etc/X11/xorg.conf.d
 sudo cp etc/X11/xorg.conf.d/* /etc/X11/xorg.conf.d/
 
+########################################
+# INSTALL XINIT
+########################################
+
+cp .xinitrc ~
+
+########################################
+# DONE
+########################################
+
 echo ""
-echo "Done!"
-echo ""
-echo "Make sure repository is located at:"
-echo "~/arch-i3-rice"
-echo ""
-echo "Wallpaper path depends on this location."
+echo "Setup complete."
+echo "Reboot recommended."
